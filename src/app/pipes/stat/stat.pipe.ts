2002219fe2bdb2e1ed95dd348a70cedbbc54391b
daniromo <dev@daniromo.me>
2023-02-02 15:08:30 -0600

    Added crafted netherite stat 🪙

--- src/app/pipes/stat/stat.pipe.ts
+++ src/app/pipes/stat/stat.pipe.ts
@@ -9,6 +9,7 @@ export class StatPipe implements PipeTransform {
     switch (value) {
       case 'minecraft:play_time': return $localize`Time played`
       case 'minecraft:deaths': return $localize`Deaths`
+      case 'minecraft:netherite_ingot': return $localize`Crafted netherite`
       case 'minecraft:traded_with_villager': return $localize`Traded with villager`
       default: return value
     }
