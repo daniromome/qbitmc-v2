5b5fe1b8f09090077ab43f3a70afda99acf2cce0
daniromo <dev@daniromo.me>
2023-02-02 21:38:12 -0600

    Fixed netherite stat so it's not exploitable 🧠

--- src/app/pipes/stat/stat.pipe.ts
+++ src/app/pipes/stat/stat.pipe.ts
@@ -9,7 +9,7 @@ export class StatPipe implements PipeTransform {
     switch (value) {
       case 'minecraft:play_time': return $localize`Time played`
       case 'minecraft:deaths': return $localize`Deaths`
-      case 'minecraft:netherite_ingot': return $localize`Crafted netherite`
+      case 'minecraft:netherite_scrap': return $localize`Crafted netherite scraps`
       case 'minecraft:traded_with_villager': return $localize`Traded with villager`
       default: return value
     }
