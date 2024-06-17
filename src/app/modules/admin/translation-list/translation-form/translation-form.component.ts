6f03d3f086d58d2c5048450422bcbd0e59973067
Daniel Sánchez <dev@daniromo.me>
2024-06-17 11:07:00 -0600

    Translation Record

--- src/app/modules/admin/translation-list/translation-form/translation-form.component.ts
+++ src/app/modules/admin/translation-list/translation-form/translation-form.component.ts
@@ -1,6 +1,11 @@
 import { Component, effect, inject } from '@angular/core'
+import { FormGroup, FormRecord, NonNullableFormBuilder } from '@angular/forms'
 import { Store } from '@ngrx/store'
+import { LOCALE, Locale, Translation } from '@qbitmc/common'
 import { selectRouteParam } from '@store/router'
+import { FormFrom } from '@utils'
+
+type TranslationForm = FormGroup<FormFrom<Translation>>
 
 @Component({
   selector: 'qbit-translation-form',
@@ -11,10 +16,24 @@ import { selectRouteParam } from '@store/router'
 })
 export class TranslationFormComponent {
   private readonly store = inject(Store)
+  private readonly fb = inject(NonNullableFormBuilder)
 
   public readonly key = this.store.selectSignal(selectRouteParam('key'))
 
+  public readonly form: FormRecord<TranslationForm> = Object.values(LOCALE).reduce((record, cur) => {
+    record.addControl(cur, this.newTranslationGroup(cur))
+    return record
+  }, this.fb.record<TranslationForm>({}))
+
   public constructor() {
     effect(() => console.log(this.key()))
+  }
+
+  private newTranslationGroup(locale: Locale): TranslationForm {
+    return this.fb.group({
+      key: '',
+      value: '',
+      locale
+    })
   }
 }
