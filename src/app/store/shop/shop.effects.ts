1a76c0f3318f7a018776cd7083cb3e773a7c7a75
Dani Romo <dev@daniromo.me>
2024-06-18 21:11:42 -0600

    Fixed build issues

--- src/app/store/shop/shop.effects.ts
+++ src/app/store/shop/shop.effects.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core'
-import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { concatLatestFrom } from '@ngrx/operators'
 import { catchError, map, of, from, zip } from 'rxjs'
 import { ShopActions } from '@store/shop'
 import { repeat, switchMap, tap } from 'rxjs/operators'
